cd284d580c4ed75155536374cb18bd15c95c8b34
+ <+>
2022-11-03 12:08:54 -0500

    added header

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <h2> Weather Search Engine </h2>
     <App />
   </React.StrictMode>
 );
